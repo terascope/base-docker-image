595f88b20c4cfd57f67c908dbb921e25963fd02e
peterdemartini <thepeterdemartini@gmail.com>
2020-01-23 09:20:30 -0700

    compress image

--- build.sh
+++ build.sh
@@ -3,7 +3,13 @@
 set -e
 
 printf '\n* BUILDING node:10.18.1...\n\n'
-docker build --build-arg 'NODE_VERSION=10.18.1' --tag terascope/node-base:10.18.1 .
+docker build \
+    --build-arg 'NODE_VERSION=10.18.1' \
+    --compress \
+    --tag terascope/node-base:10.18.1 .
 
 printf '\n* BUILDING node:12.14.1...\n\n'
-docker build --build-arg 'NODE_VERSION=12.14.1' --tag terascope/node-base:12.14.1 .
+docker build \
+    --build-arg 'NODE_VERSION=12.14.1' \
+    --compress \
+    --tag terascope/node-base:12.14.1 .
