9838bcd02c68c7021d73bbdd825ed7ab1bf2eb39
Austin Godber <godber@uberhip.com>
2023-02-22 14:35:24 -0700

    only build the core image, kafka connector no longer needed

--- build-and-push.sh
+++ build-and-push.sh
@@ -92,9 +92,9 @@ main() {
         exit 1
     fi
 
-    local versions=("14.19.3" "16.15.0" "18.2.0")
+    local versions=("14.21.3" "16.19.1" "18.14.2")
     for version in "${versions[@]}"; do
-        docker_build "$registry" "$version"
+        # docker_build "$registry" "$version"
         docker_build "$registry" "$version" "-core"
     done
 
@@ -103,7 +103,7 @@ main() {
     prompt "Do you want to push $image_tag?" || exit 0
 
     for version in "${versions[@]}"; do
-        docker_push "$registry" "$version"
+        # docker_push "$registry" "$version"
         docker_push "$registry" "$version" "-core"
     done
 }
